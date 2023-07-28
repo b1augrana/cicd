c58ac3555ec0350ee20ecf684e40de55e41b6279
Ivan Oleynik <ivo_oleynik@yahoo.com>
2023-07-28 18:49:53 +0300

    tests.py

--- logistic/tests.py
+++ logistic/tests.py
@@ -5,7 +5,7 @@ from rest_framework.test import APIClient
 
 class TestView(TestCase):
     def test_response(self):
-        url = "/api/v1/test/"
+        url = "/api/v1/"
         client = APIClient()
         response = client.get(url)
         self.assertEqual(response.status_code, 200)
